f1cc6fcda11bd09eb0f4237e734da66f9e84f90d
towergame <h@h.h>
2021-03-14 19:19:52 +0200

    Work on marker selection

--- client/src/ts/map.ts
+++ client/src/ts/map.ts
@@ -1,20 +1,120 @@
 import * as L from "leaflet";
 import "leaflet/dist/leaflet.css";
+import cumfuck from "img/cumfuck.png";
+import cumfuckActive from "img/cumfuckActive.png";
 
-export class Map{
-	map: L.Map
+class MapObject {
+	latLng: L.LatLngExpression;
+	description: string;
 
-	constructor(id:string){
+	constructor(coord: L.LatLngExpression, desc: string) {
+		this.latLng = coord;
+		this.description = desc;
+	}
+}
+
+export class Map {
+	map: L.Map;
+	markers: L.Marker[] = [];
+	currentlyActive: L.Marker[] = [];
+
+	iconInactive = new L.Icon({
+		iconUrl: cumfuck,
+		iconAnchor: L.Icon.Default.prototype.options.iconAnchor
+	});
+	iconActive = new L.Icon({
+		iconUrl: cumfuckActive,
+		iconAnchor: L.Icon.Default.prototype.options.iconAnchor
+	});
+
+	nonMetricDistanceTo(thisLatLng: L.LatLngExpression, otherLatLng: L.LatLngExpression) {
+		let a = otherLatLng as L.LatLng;
+		let b = otherLatLng as L.LatLng;
+		return Math.sqrt((a.lat - b.lat) ** 2 + (a.lng - b.lng) ** 2);
+	}
+
+	constructor(id: string) {
+
 		this.map = L.map(id, {
-			center: [51.505, -0.09],
-			zoom: 8
+			center: [56.50475439537235, 21.010924221837993], //Liepaaja be like: [56.50475439537235, 21.010924221837993]
+			zoom: 15,
+			minZoom: 13,
+			maxBounds: L.latLngBounds(
+				L.latLng(56.46671450384127, 20.942759323837972),
+				L.latLng(56.56357049044075, 21.106128222201132)
+			)
 		});
-	
+
+		this.map.addEventListener("contextmenu", (e) => {
+			this.clearSelection();
+		});
+
+		this.map.addEventListener("keydown", (e) => {
+			let keyEv = e as L.LeafletKeyboardEvent;
+			if (keyEv.originalEvent.key == "shift") {
+				this.map.removeEventListener("contextmenu");
+			}
+		});
+
+		this.map.addEventListener("keyup", (e) => {
+			let keyEv = e as L.LeafletKeyboardEvent;
+			if (keyEv.originalEvent.key == "shift") {
+				this.map.addEventListener("contextmenu", (e) => {
+					this.clearSelection();
+				});
+			}
+		});
+
 		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 			maxZoom: 19,
 			attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
 		}).addTo(this.map);
-	
+
 		L.control.scale().addTo(this.map);
+
+		let objects = [
+			new MapObject([56.51292213894928, 21.012326233692708], "This is a river of cum, click if you dare."),
+			new MapObject([56.51251995960712, 21.028135815168024], "The fish zone"),
+			new MapObject([56.518912681668304, 20.98500589524165], "Plce 3 with no joke!!!")
+		] // Hand defined for now
+
+		objects.forEach(element => {
+			let testMarker = L.marker(element.latLng, {
+				icon: this.iconInactive
+			}).addTo(this.map);
+
+			testMarker.bindTooltip(element.description, {
+				offset: new L.Point(0, -30)
+			});
+
+			testMarker.addEventListener("click", (e) => {
+				this.activate(testMarker);
+			});
+
+			this.markers.push(testMarker);
+		});
+	}
+
+	activate(marker: L.Marker) {
+		if (this.currentlyActive.includes(marker)) {
+			if (this.currentlyActive.length > 1) {
+				this.currentlyActive[0].setIcon(this.iconInactive);
+				this.currentlyActive.shift();
+			}
+			return;
+		}
+		if (this.currentlyActive.length == 2) {
+			this.currentlyActive[0].setIcon(this.iconInactive);
+			this.currentlyActive.shift();
+		}
+		marker.setIcon(this.iconActive);
+		this.currentlyActive.push(marker);
+	}
+
+	clearSelection() {
+		this.currentlyActive.forEach(marker => {
+			marker.setIcon(this.iconInactive);
+		});
+		this.currentlyActive = [];
 	}
 }
