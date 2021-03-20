68feb7aff78a3e3c85cec0c5a6fbc8e3e78e7115
Kristians <vintersk@gmail.com>
2021-03-20 23:29:03 +0200

    fog of war bugger fixer

--- client/src/ts/map/map.ts
+++ client/src/ts/map/map.ts
@@ -60,10 +60,10 @@ export default class Map {
 			scrollWheelZoom: false,
 			doubleClickZoom: false,
 			// minZoom: 13,
-			maxBounds: L.latLngBounds(
+			/*maxBounds: L.latLngBounds(
 				L.latLng(56.47, 20.95),
 				L.latLng(56.56, 21.1)
-			)
+			)*/
 		});
 
 		this.map.addEventListener("contextmenu", (e) => {
